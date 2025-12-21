937cda829149054035b4886267e235019729ee25
elimfilters <221251498+elimfilters@users.noreply.github.com>
2025-12-21 14:08:12 -0600

    Create marineAuthorities.js

---
+++ src/config/marineAuthorities.js
@@ -0,0 +1,8 @@
+module.exports = Object.freeze({
+  ORDER: ['RACOR', 'PARKER', 'SIERRA'],
+  CANONICAL: {
+    RACOR: 'RACOR',
+    PARKER: 'RACOR',   // Parker = Racor
+    SIERRA: 'SIERRA'
+  }
+});
