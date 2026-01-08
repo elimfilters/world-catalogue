455934c6af2f65a7784aa38977e3aede5122b005
Victor Abreu <victor.abreu@elimfilters.com>
2026-01-08 02:58:17 -0600

    chore: add debug routes endpoint

---
+++ controllers/debugRoutes.controller.js
@@ -0,0 +1,21 @@
+﻿module.exports = function debugRoutesController(req, res) {
+    try {
+        const routes = [];
+        req.app._router.stack.forEach((middleware) => {
+            if (middleware.route) {
+                const methods = Object.keys(middleware.route.methods).join(",");
+                routes.push({ path: middleware.route.path, methods });
+            } else if (middleware.name === "router") {
+                middleware.handle.stack.forEach((handler) => {
+                    if (handler.route) {
+                        const methods = Object.keys(handler.route.methods).join(",");
+                        routes.push({ path: handler.route.path, methods });
+                    }
+                });
+            }
+        });
+        return res.json({ success: true, routes });
+    } catch (err) {
+        return res.status(500).json({ success: false, error: err.message });
+    }
+};
